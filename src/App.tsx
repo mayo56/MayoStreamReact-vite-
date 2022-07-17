1866f04513c4d0bb5e93e2c7ed093874d6d71cd6
mayo56 <maxinou56.lr@gmail.com>
2022-07-17 16:05:57 +0200

    Ajout première page + routes de chemin

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
+import { Routes, Route } from "react-router-dom"
+import Home from './Pages/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <p>LOL</p>
+    <div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
     </div>
   )
 }
