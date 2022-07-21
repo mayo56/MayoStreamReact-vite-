2130dd16734c01b8ee7acc0d64dbb757c7fb3046
mayo56 <maxinou56.lr@gmail.com>
2022-07-21 18:50:23 +0200

    Ajout formulaire d'ajout de vidéo (non terminé)

---
+++ src/Components/AddVideos/VideoForm.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+const VideoForm = () => {
+    const [title, setTitle] = useState<string>("");
+
+    return (
+        <div className='mt-[10px] text-center'>
+            <h1 className='font-bold text-[30px]'>Ajouter une vidéo ! Trop cool !!</h1>
+            <br />
+            <h6 className='text-[20px]'>Nom de la vidéo:<strong className='text-red-500'>*</strong></h6>
+            <input type="text" className='text-black p-[5px] rounded-lg'
+            value={title} onChange={(e) => setTitle(e.target.value)} />
+        </div>
+    );
+};
+
+export default VideoForm;
--- src/Pages/AddVideo.tsx
+++ src/Pages/AddVideo.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import VideoForm from '../Components/AddVideos/VideoForm';
+import NavBar from '../Components/Home/NavBar';
 
 const AddVideo = () => {
     return (
-        <div>
-            
+        <div className='text-white'>
+            <NavBar />
+            <VideoForm />
         </div>
     );
 };
